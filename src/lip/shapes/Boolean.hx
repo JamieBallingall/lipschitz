3350bf7ad8dcf9bc10a0ec203a38913fdb5dd351
JamieBallingall <jamie.ballingall@gmail.com>
2017-02-14 10:06:50 -0700

    Additional Boolean operations

--- src/lip/shapes/Boolean.hx
+++ src/lip/shapes/Boolean.hx
@@ -6,4 +6,22 @@ class Boolean {
       return Math.min(a(xx, yy), b(xx, yy));
     }
   }
+
+  public static function intersection(a: Shape, b: Shape): Shape {
+    return function (xx: Float, yy: Float): Float {
+      return Math.max(a(xx, yy), b(xx, yy));
+    }
+  }
+
+  public static function difference(a: Shape, b: Shape): Shape {
+    return function (xx: Float, yy: Float): Float {
+      return Math.max(a(xx, yy), -b(xx, yy));
+    }
+  }
+
+  public static function complement(a: Shape): Shape {
+    return function (xx: Float, yy: Float): Float {
+      return -a(xx, yy);
+    }
+  }
 }
